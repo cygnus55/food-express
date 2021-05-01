e1737540c6aa72f86282086baed27287b2183bb2
Deependra12 <dg02432018@student.ku.edu.np>
2021-05-01 06:03:20 +0545

    change in home page

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from restaurants.models import Restaurant
 from django.template import RequestContext
 
 
@@ -9,6 +9,13 @@ from django.template import RequestContext
 
 class HomePageView(TemplateView):
     template_name = 'base/home.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        restaurants = Restaurant.objects.all()[:3]
+        context['restaurants'] = restaurants
+        return context
+
 
 
 def handler404(request, *args, **argv):
