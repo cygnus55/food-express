1c704c20219611df7878cc3979e923149e3c91da
RamrajCh <rc02431618@student.ku.edu.np>
2021-07-05 19:24:18 +0545

    buy now

--- coupons/urls.py
+++ coupons/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path
 from . import views
 
@@ -8,4 +9,5 @@ app_name = 'coupons'
 urlpatterns = [
     path('apply/', views.coupon_apply, name='apply'),
     path('unapply/', views.coupon_unapply, name='unapply'),
+    path('ajax-verify-coupon/', views.verify_coupon, name='ajax_verify_coupon'),
 ]
